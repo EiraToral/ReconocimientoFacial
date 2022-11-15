d5fdcb5183c547631def58dfd151422c6ba436d3
EiraToral <eiratoral.buap@gmail.com>
2022-11-15 14:06:40 -0600

    Se reemplazan los valores y carácteres de los datos para normalizarlos con 1 y 0

--- CargaCalebA.py
+++ CargaCalebA.py
@@ -8,13 +8,17 @@ import datetime
 
 np.set_printoptions(precision=4)
 
-#Esto cambia el doble espacio entre los datos de la tabla de CalebA
-with open('list_attr_celeba.txt', 'r') as f:
+#Se carga el recuadro gráfico data framme con los espacios corregidos
+df = pd.read_csv('../../Downloads/attr_celeba_prepared.txt', sep =' ', header = None)
+#print(df) #para observar el dataset corregido
+#print(df)
+#print (df[0].head) #Imprime los primeros elementos (.head) de la primera columna (df[0])
 
-    print("skipping : " + f.readline())
-    print("skipping headers : " + f.readline())
-    with open('attr_celeba_prepared.txt' , 'w') as newf:
-        for line in f:
-            new_line = ' '.join(line.split())
-            newf.write(new_line)
-            newf.write('\n')
+#A contunuación se configuran los valores del dataset df para que sus valores sean únicamente 0 y 1
+df.replace(to_replace=-1, value=0, inplace=True) #Reemplaza los valores "-1" por 0
+df.replace(to_replace='0', value=0, inplace=True) #Reemplaza los caracteres '0' por 0
+df.replace(to_replace='-1', value=0, inplace=True) #Reemplaza los carateres '1' por 1
+df.replace(to_replace='1', value=1, inplace=True) #Reemplaza los caracteres '1' por 1
+#Se toman todas las filas y a partir de la segunda columna del dataframe y se transforma a una matriz numpy
+x = np.asarray(df.iloc[:, 1:]).astype('int64')
+#print(x)
