2e7db2570b29870ba4e794718b3d656aa826b9f1
EiraToral <eiratoral.buap@gmail.com>
2022-11-15 13:43:36 -0600

    Se eliminan los dobles espacios de los datos de CalebA lo que genera un nuevo dataset en formato txt

---
+++ CargaCalebA.py
@@ -0,0 +1,20 @@
+import pathlib
+import os
+import matplotlib.pyplot as plt
+import pandas as pd
+import numpy as np
+import tensorflow as tf
+import datetime
+
+np.set_printoptions(precision=4)
+
+#Esto cambia el doble espacio entre los datos de la tabla
+with open('list_attr_celeba.txt', 'r') as f:
+
+    print("skipping : " + f.readline())
+    print("skipping headers : " + f.readline())
+    with open('attr_celeba_prepared.txt' , 'w') as newf:
+        for line in f:
+            new_line = ' '.join(line.split())
+            newf.write(new_line)
+            newf.write('\n')
